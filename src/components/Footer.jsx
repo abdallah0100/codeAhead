fc5af8a68690418a6fee0b4fab046b42e333d384
Abdallah <abdlaa1912@gmail.com>
2022-10-31 23:25:46 +0200

    Finished header and footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,22 @@
+import './footer.css';
 function Footer(){
     return(
-        <div>My footer!</div>
+        <div className="footer-container">
+            <div className="socials">
+                <h2>CodeAhead</h2>
+                <label>Visit us at:</label> <br />
+                <img src="/images/footer/twitter.jpg" alt='Twitter icon' id='twitter' />
+                <img src="/images/footer/Instagram-Icon.png" alt='Instagram icon' id='instagram'/>
+                <img src="/images/footer/facebook.png" alt='Facebook icon' id='facebook'/>
+            </div>
+            <hr />
+            <div className='copyRights'>
+                <i>
+                    <p>© 2022 CodeAhead</p>
+                    <p>Contact us: info@codeahead.com</p>
+                </i>
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,9 @@
+function Logo(){
+    return (
+        <a href="/" id="logo">
+            <label className="title">CodeAhead</label><br />
+            <label className="motto">Craft apps to perfection</label>
+        </a>
+    );
+}
+export default Logo;
