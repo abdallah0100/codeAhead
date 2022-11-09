cea49628135c995adfa2f3cba6754d58e48e2626
Abdallah <abdlaa1912@gmail.com>
2022-11-09 15:28:22 +0200

    front end for profile box, background and placeholder landing

---
+++ src/components/home/About.jsx
@@ -0,0 +1,16 @@
+function About(){
+    return(
+        <div className="content_block" id='about'>
+            <h1>About us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+             Integer dictum, nulla lacinia facilisis viverra, magna velit dapibus augue, non convallis diam eros ut ante.
+              Sed et lectus dignissim, blandit massa vitae, rutrum ante.
+               In sapien lorem, sollicitudin vel cursus sed, vulputate ut enim.
+                Nam in nulla enim. Fusce tristique tincidunt augue, sed egestas odio rhoncus vel.
+                 Suspendisse consectetur lacus a arcu placerat, id molestie nulla porta.
+                  Sed facilisis mauris eget sagittis finibus.</p>
+        </div>
+    );
+}
+
+export default About;
