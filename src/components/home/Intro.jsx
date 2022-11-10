8200514d97566fbe63783c560462676f75192d36
Abdallah <abdlaa1912@gmail.com>
2022-11-10 22:44:14 +0200

    added an intro section

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,14 +1,11 @@
 function Intro(){
     return(
-    <div className="content_block">
-        <h1>Intro</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-             Integer dictum, nulla lacinia facilisis viverra, magna velit dapibus augue, non convallis diam eros ut ante.
-              Sed et lectus dignissim, blandit massa vitae, rutrum ante.
-               In sapien lorem, sollicitudin vel cursus sed, vulputate ut enim.
-                Nam in nulla enim. Fusce tristique tincidunt augue, sed egestas odio rhoncus vel.
-                 Suspendisse consectetur lacus a arcu placerat, id molestie nulla porta.
-                  Sed facilisis mauris eget sagittis finibus.</p>
+    <div className="content_block" id="home">
+        <p className="welcome_msg">Welcome to <label className="website_name">CodeAhead</label>, <br />
+        We're excited for you to join our<br />
+        community of developers.</p>
+        <button className="joinUs_btn">Join us now!</button>
+        <p className="welcome_msg register_msg">Already registered? click <a href="/#">here</a> to log in</p>
 
     </div>);
 }
