3d432f77e32271a59b1177d5cf17aad3e35c08a1
Abdallah <abdlaa1912@gmail.com>
2022-12-05 21:45:50 +0200

    fixed width issue, finished website design w. bootstrap

--- src/components/home/Contact.jsx
+++ src/components/home/Contact.jsx
@@ -1,9 +1,15 @@
+import React from "react";
 import { Button, Container, FloatingLabel, Form } from "react-bootstrap";
 
 function Contact(){
+    const [hideOther, updateOther] = React.useState(true);
+    const handleRadioChange = (e)=> {
+        const other = document.getElementById("other_topic");
+        updateOther(!other.checked);
+    };
 
     return(
-        <Container className="landing_content contact_container">
+        <Container className="landing_content contact_container" id="contact">
             <h2>Contact Us</h2>
             <center><h5>We are pleased to hear from You regarding any matter!</h5></center>
             <Form>
@@ -15,16 +21,16 @@ function Contact(){
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" placeholder="example@domain.com" />
                 </Form.Group>
-                <Form.Group><br />
+                <Form.Group onChange={handleRadioChange}><br />
                     <Form.Label>Topic</Form.Label><br />
                     <Form.Check type="radio" label="suggestion" name="contact_topic" inline />
                     <Form.Check type="radio" label="Report a bug" name="contact_topic" inline />
                     <Form.Check type="radio" label="Business" name="contact_topic" inline />
-                    <Form.Check type="radio" label="other" name="contact_topic" inline />
-                    <Form.Control type="text" id='other_text' />
-                </Form.Group>
-                <FloatingLabel label="Contact message" className="mb-3" controlId="messageContent">
-                    <Form.Control as="textarea" rows={5} placeholder="message content" />
+                    <Form.Check type="radio" label="other" name="contact_topic" id="other_topic" inline />
+                    <Form.Control type="text" id='other_text' placeholder="Topic specification" hidden={hideOther} />
+                </Form.Group><br /><br />
+                <FloatingLabel label="Contact message" className="mb-3 contact_msg" controlId="messageContent">
+                    <Form.Control as="textarea" placeholder="message content" />
                 </FloatingLabel>
                 <Form.Group className="contact_buttons">
                     <Button variant='success' id='contact_send'>Send</Button>
